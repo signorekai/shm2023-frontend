fb3867a62794da69cf99dcbce3eae84cc7b07ae2
Alfred Lau <alfred@sulphur.com.sg>
2023-05-03 15:32:26 +0800

    :sparkles: added getNodeByURL

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,56 @@
-export const fetchGQL = async (query) => {
+export const fetchGQL = async (query, variables = {}) => {
   const response = await fetch("https://app.superherome.sg/v2/graphql", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       query,
+      variables
     }),
   });
   const result = await response.json();
   return result;
+}
+
+export const getNodeByURL = async (url) => {
+  const { data } = await fetchGQL(`
+    query GetNodeByURI($uri: String!) {
+      nodeByUri(uri: $uri) {
+        __typename
+        isContentNode
+        isTermNode
+        ... on Page {
+          id
+          template {
+            templateName
+          }
+        }
+        ... on Post {
+          id
+          title
+          date
+          uri
+          excerpt
+          content
+          categories {
+            nodes {
+              name
+              uri
+            }
+          }
+          featuredImage {
+            node {
+              mediaItemUrl
+              altText
+            }
+          }
+        }
+      }
+    }
+  `,
+ {
+    uri: url
+}
+  );
+
+  return data;
 }
