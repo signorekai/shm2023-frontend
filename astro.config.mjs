3bab3cfb5eb5d317ee28769ea2c2cae9bace6e3e
Alfred Lau <alfred@sulphur.com.sg>
2023-07-06 19:45:36 +0800

    :wrench: minified html

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  compressHTML: true,
   output: "server",
   adapter: vercel(),
   integrations: [tailwind({
