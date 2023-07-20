0b7a03da5c8e7014ff5110a8f29fb64cf398982c
Alfred Lau <alfred@sulphur.com.sg>
2023-07-20 16:46:57 +0800

    :heavy_plus_sign: added astro-preload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preload from "astro-preload";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind({
+  integrations: [preload(), tailwind({
     config: {
       path: './tailwind.config.cjs',
       applyBaseStyles: false
