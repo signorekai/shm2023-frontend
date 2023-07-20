d5206c4cc87d8466ef63e947fbec4b09ba427989
Alfred Lau <alfred@sulphur.com.sg>
2023-07-20 18:37:10 +0800

    :bug: remove astro-preload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import vercel from "@astrojs/vercel/serverless";
 
-import preload from "astro-preload";
-
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   output: "server",
   adapter: vercel(),
-  integrations: [preload(), tailwind({
+  integrations: [tailwind({
     config: {
       path: './tailwind.config.cjs',
       applyBaseStyles: false
